7a8e04fd9e09f9ed8a296f6de511391475f698c0
Corey Staten <corey.staten@hpe.com>
2022-09-29 14:01:17 -0400

    Fix __init__.py

--- examples/ray/notebook/ray_determined/__init__.py
+++ examples/ray/notebook/ray_determined/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-from ray_determined._ray import init_ray_determined
-=======
 from ray_determined._ray import init_ray_determined
 from ray_determined.launch_ray import *
 from ray_determined.distributed_actor import *
->>>>>>> Stashed changes
